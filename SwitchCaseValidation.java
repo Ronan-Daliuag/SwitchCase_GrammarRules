d999597d712f4e31428b634dce073a8b04459c45
Angelo Daniel Dela Paz <angelodaniel.delapaz.cics@ust.edu.ph>
2023-10-04 22:32:23 +0800

    From switch to case ():

--- SwitchCaseValidation.java
+++ SwitchCaseValidation.java
@@ -1,32 +1,72 @@
+import java.util.Scanner;
+
 public class SwitchCaseValidation {
     public static void main(String[] args) {
-        String input = "switch(test)(case 1:System.out.println(\"1\");break;" +
-                "case: 10:System.out.println(\"10\");break;}";
+        Scanner sc = new Scanner(System.in);
+        /*
+         * System.out.println("Enter Input: ");
+         * String iinput = sc.nextLine();
+         */
+
+        String input = "switch(test){case 1: System.out.println(\"1\"); \n break;\n}";
         System.out.println("Input: " + input);
+
         String forCheck = "" + input.charAt(0) + input.charAt(1) + input.charAt(2) +
                 input.charAt(3) + input.charAt(4) + input.charAt(5) + input.charAt(6);
-        if (forCheck.toLowerCase().equals("switch(")){
-            int mark = 8;
-            String temp = "temp";
-            for (int i = mark; temp != ""; i++){
-                if (i == input.length()){
-                    System.out.println("This is not a valid loop declaration");
-                    System.exit(0);
+
+        if (forCheck.toLowerCase().equals("switch(")) {
+            System.out.println("GOODS SA switch");
+
+            int pointer = 8;
+            for (int i = pointer; i < input.length(); i++) {
+
+                if (input.charAt(i) == ')' && input.charAt(i + 1) == '{') {
+                    System.out.println("goods sa ){");
+                    continue;
                 }
-                temp = "" + input.charAt(i);
-                if (temp.equals(")")){
-                    mark = i;
+
+                if (input.charAt(i) == 'c' && input.charAt(i + 1) == 'a' && input.charAt(i + 2) == 's'
+                        && input.charAt(i + 3) == 'e') {
+                    System.out.println("goods sa case");
+                    i += 3;
+                    pointer = i;
                     break;
                 }
             }
-            mark++;
-            temp = "temp";
-            for (int i = mark; temp != ""; i++) {
-                if (i == input.length()){
-                    System.out.println("This is not a valid loop declaration");
-                    System.exit(0);
+
+            for (int i = pointer; i < input.length(); i++) {
+                if ((input.charAt(i) >= 'a' && input.charAt(i) <= 'z') || (input.charAt(i) >= 0 && input.charAt(i) <= 9) && (input.charAt(i+1) == ':')) {
+                    System.out.println("goods sa case number or char at colon");
+                    break;
                 }
             }
+
+            /*
+             * int mark = 8;
+             * String temp = "temp";
+             * for (int i = mark; temp != ""; i++) {
+             * if (i == input.length()) {
+             * System.out.println("This is not a valid loop declaration");
+             * System.exit(0);
+             * }
+             * temp = "" + input.charAt(i);
+             * if (temp.equals(")")) {
+             * mark = i;
+             * break;
+             * }
+             * }
+             * 
+             * mark++;
+             * temp = "temp";
+             * for (int i = mark; temp != ""; i++) {
+             * if (i == input.length()) {
+             * System.out.println("This is not a valid loop declaration");
+             * System.exit(0);
+             * }
+             * }
+             */
+        } else {
+            System.out.println("This is not a valid switch declaration");
         }
     }
 }
