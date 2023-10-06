17566ef802d0b2eb44c7547cdc54c33c8fae0e9e
TyranoLegend <147166088+TyranoLegend@users.noreply.github.com>
2023-10-06 23:50:40 +0800

    <var>){ update

--- SwitchCaseValidation.java
+++ SwitchCaseValidation.java
@@ -13,28 +13,62 @@ public class SwitchCaseValidation {
 
         String forCheck = "" + input.charAt(0) + input.charAt(1) + input.charAt(2) +
                 input.charAt(3) + input.charAt(4) + input.charAt(5) + input.charAt(6);
-
-        if (forCheck.toLowerCase().equals("switch(")) {
+        
+        if (forCheck.toLowerCase().equals("switch(")) { //I think kailangan tanggalin yung .toLowerCase() since I think explicit the small cases yung term na "switch"
             System.out.println("GOODS SA switch");
 
             //Checking the <var> inside the () of switch() is empty or not
-            int pointer = 7;
-            if(input.charAt(pointer) == ')') {
+            if(input.charAt(7) == ')') {
                 System.out.println("<var> can be empty");
                 System.exit(0);
             }
- 
+
+            int pointer = 7;
+            String beforevar ="";
+            String var = "";
+            String empty ="";
+            //(<var>){ Checker
             for (int i = pointer; i < input.length(); i++) {
-
+                if(input.charAt(i) == ' '){
+                    System.out.println("empty");
+                    empty = empty + input.charAt(i);
+                    beforevar = beforevar + input.charAt(i);
+                    continue;
+                }              
                 //Increasing the pointer by the number of characters <var> has
-                if(input.charAt(i) != ')') {
+                if(input.charAt(i) != ')' && input.charAt(i) != ' ' ) {
+                    beforevar = "l";
+                    var = var + input.charAt(i);//Put the character in the String Checker
+                        if(input.charAt(i+1) == ' ') {
+                            for(int j = i; j < input.length(); j++) {
+                                if (input.charAt(j+1) == ' '){
+                                    System.out.println("after name space");
+                                    continue;
+                                }
+                                if (input.charAt(j+1) == ')') {
+                                    break;
+                                }
+                                if (input.charAt(j+1) != ' ') {
+                                        System.out.println("<var> must be a singlular name");
+                                        System.exit(0);  
+                                }
+                            }
+                        }
                     continue;
                 }
-
+                if(beforevar.equals(empty)){
+                    System.out.println("Does not have <var>");
+                    break;
+                }
                 if (input.charAt(i) == ')' && input.charAt(i + 1) == '{') {
-                    System.out.println("goods sa ){");
-                    continue;
+                    System.out.println(var + "\nreached");
+                    pointer = i + 2; // brings to pointer after {
+                    break;
                 }
+            }
+
+            //case: checker
+            for (int i = pointer; i < input.length(); i++) {
 
                 if (input.charAt(i) == 'c' && input.charAt(i + 1) == 'a' && input.charAt(i + 2) == 's'
                         && input.charAt(i + 3) == 'e') {
