9b8b160e90d3e64b07ebb51bc74f2fc08b026b78
Shaun-PJuan <shaunkristoffer.padrejuan.cics@ust.edu.ph>
2023-10-16 01:31:09 +0800

    Added methods for checking the switch keyword and its variable

--- SwitchCaseValidation.java
+++ SwitchCaseValidation.java
@@ -1,118 +1,180 @@
 import java.util.Scanner;
 
 public class SwitchCaseValidation {
+//    Method to check for switch reserved word. Returns true if there is an instance of the word
+//    Return false if no instance is found
+    public static boolean switchChecker(String input){
+        String switchFinder = "";
+        for (int i = 0; i<input.length(); i++){
+            if (switchFinder.equals("switch"))
+                return true;
+            else
+                switchFinder = switchFinder + input.charAt(i);
+        }
+        return false;
+    }
+//    Method to check the variable inside the switch()
+//    Note: switchChecker must be true to test this next
+    public static boolean switchVariableChecker(String input){
+//        Finds the first instance of open parenthesis
+        String switchVariableFinder = "";
+        int firstParenthesis = 0;
+        for (int i = 0; i < input.length(); i++){
+            if (input.charAt(i) == '('){
+                firstParenthesis = i + 1; //does not include the opening parenthesis
+                break;
+            }
+            if (input.charAt(i) == ')'){
+                return false; // no opening parenthesis found before closing
+            }
+        }
+//        Loop that starts from the index of the first parenthesis after the switch keyword
+        if (firstParenthesis == 0)
+            return false; // no opening parenthesis
+        for (int i = firstParenthesis; i < input.length();i++) {
+            if (input.charAt(i) == ')'){
+                break;
+            }
+            switchVariableFinder = switchVariableFinder + input.charAt(i);
+        }
+//        if the <var> is just whitespace or is null
+        if (switchVariableFinder.isBlank())
+            return false; //no variable found in argument
+        System.out.println(switchVariableFinder);
+//      if the <var> has illegal characters inside parenthesis
+//      TODO: add more illegal characters, functions inside parenthesis?
+        if (!switchVariableFinder.isBlank()){
+            for (int i = 0; i < switchVariableFinder.length(); i++){
+                if (switchVariableFinder.charAt(i) == '{' || switchVariableFinder.charAt(i) == '}'
+                        || switchVariableFinder.charAt(i) == '(' || switchVariableFinder.charAt(i) == ')')
+                    return false; // illegal character found
+            }
+            return true; // valid variable argument
+        }
+        return false; //<var> most likely does not have a closing parenthesis to the opening parenthesis
+    }
+    //TODO: implement these methods
+    public static boolean curlyBraceChecker (String input){
+        return false;
+    }
+    public static boolean caseAndBreakChecker (String input){
+        return false;
+    }
+    public static boolean defaultChecker (String input){
+        return false;
+    }
+    public static boolean endingCurlyBraceChecker (String input){
+        return false;
+    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        /*
-         * System.out.println("Enter Input: ");
-         * String iinput = sc.nextLine();
-         */
-        // TODO function: add checking for break statements. Print out whether there is a break statement before cases
-        // TODO function: checking for default, recognize whether default block is present
         String input = "switch(test){case 1: System.out.println(\"1\"); \n break;\n}";
         System.out.println("Input: " + input);
+        if (!switchVariableChecker(input))
+            System.out.println("Invalid switch variable");
+        if (switchChecker(input) && switchVariableChecker(input))
+            System.out.println("The input is a valid switch statement");
 
-        String forCheck = "" + input.charAt(0) + input.charAt(1) + input.charAt(2) +
-                input.charAt(3) + input.charAt(4) + input.charAt(5) + input.charAt(6);
-        
-        if (forCheck.toLowerCase().equals("switch(")) { //I think kailangan tanggalin yung .toLowerCase() since I think explicit the small cases yung term na "switch"
-            System.out.println("GOODS SA switch");
-
-            //Checking the <var> inside the () of switch() is empty or not
-            if(input.charAt(7) == ')') {
-                System.out.println("<var> can be empty");
-                System.exit(0);
-            }
-
-            int pointer = 7;
-            String beforevar ="";
-            String var = "";
-            String empty ="";
-            //(<var>){ Checker
-            for (int i = pointer; i < input.length(); i++) {
-                if(input.charAt(i) == ' '){
-                    System.out.println("empty");
-                    empty = empty + input.charAt(i);
-                    beforevar = beforevar + input.charAt(i);
-                    continue;
-                }              
-                //Increasing the pointer by the number of characters <var> has
-                if(input.charAt(i) != ')' && input.charAt(i) != ' ' ) {
-                    beforevar = "l";
-                    var = var + input.charAt(i);//Put the character in the String Checker
-                        if(input.charAt(i+1) == ' ') {
-                            for(int j = i; j < input.length(); j++) {
-                                if (input.charAt(j+1) == ' '){
-                                    System.out.println("after name space");
-                                    continue;
-                                }
-                                if (input.charAt(j+1) == ')') {
-                                    break;
-                                }
-                                if (input.charAt(j+1) != ' ') {
-                                        System.out.println("<var> must be a singlular name");
-                                        System.exit(0);  
-                                }
-                            }
-                        }
-                    continue;
-                }
-                if(beforevar.equals(empty)){
-                    System.out.println("Does not have <var>");
-                    break;
-                }
-                if (input.charAt(i) == ')' && input.charAt(i + 1) == '{') {
-                    System.out.println(var + "\nreached");
-                    pointer = i + 2; // brings to pointer after {
-                    break;
-                }
-            }
-
-            //case: checker
-            for (int i = pointer; i < input.length(); i++) {
-
-                if (input.charAt(i) == 'c' && input.charAt(i + 1) == 'a' && input.charAt(i + 2) == 's'
-                        && input.charAt(i + 3) == 'e') {
-                    System.out.println("goods sa case");
-                    i += 3;
-                    pointer = i;
-                    break;
-                }
-            }
-
-            for (int i = pointer; i < input.length(); i++) {
-                if ((input.charAt(i) >= 'a' && input.charAt(i) <= 'z') || (input.charAt(i) >= 0 && input.charAt(i) <= 9) && (input.charAt(i+1) == ':')) {
-                    System.out.println("goods sa case number or char at colon");
-                    break;
-                }
-            }
-
-            /*
-             * int mark = 8;
-             * String temp = "temp";
-             * for (int i = mark; temp != ""; i++) {
-             * if (i == input.length()) {
-             * System.out.println("This is not a valid loop declaration");
-             * System.exit(0);
-             * }
-             * temp = "" + input.charAt(i);
-             * if (temp.equals(")")) {
-             * mark = i;
-             * break;
-             * }
-             * }
-             * 
-             * mark++;
-             * temp = "temp";
-             * for (int i = mark; temp != ""; i++) {
-             * if (i == input.length()) {
-             * System.out.println("This is not a valid loop declaration");
-             * System.exit(0);
-             * }
-             * }
-             */
-        } else {
-            System.out.println("This is not a valid switch declaration");
-        }
+//        String forCheck = "" + input.charAt(0) + input.charAt(1) + input.charAt(2) +
+//                input.charAt(3) + input.charAt(4) + input.charAt(5) + input.charAt(6);
+//
+//        if (forCheck.toLowerCase().equals("switch(")) { //I think kailangan tanggalin yung .toLowerCase() since I think explicit the small cases yung term na "switch"
+//            System.out.println("GOODS SA switch");
+//
+//            //Checking the <var> inside the () of switch() is empty or not
+//            if(input.charAt(7) == ')') {
+//                System.out.println("<var> can be empty");
+//                System.exit(0);
+//            }
+//
+//            int pointer = 7;
+//            String beforevar ="";
+//            String var = "";
+//            String empty ="";
+//            //(<var>){ Checker
+//            for (int i = pointer; i < input.length(); i++) {
+//                if(input.charAt(i) == ' '){
+//                    System.out.println("empty");
+//                    empty = empty + input.charAt(i);
+//                    beforevar = beforevar + input.charAt(i);
+//                    continue;
+//                }
+//                //Increasing the pointer by the number of characters <var> has
+//                if(input.charAt(i) != ')' && input.charAt(i) != ' ' ) {
+//                    beforevar = "l";
+//                    var = var + input.charAt(i);//Put the character in the String Checker
+//                        if(input.charAt(i+1) == ' ') {
+//                            for(int j = i; j < input.length(); j++) {
+//                                if (input.charAt(j+1) == ' '){
+//                                    System.out.println("after name space");
+//                                    continue;
+//                                }
+//                                if (input.charAt(j+1) == ')') {
+//                                    break;
+//                                }
+//                                if (input.charAt(j+1) != ' ') {
+//                                        System.out.println("<var> must be a singlular name");
+//                                        System.exit(0);
+//                                }
+//                            }
+//                        }
+//                    continue;
+//                }
+//                if(beforevar.equals(empty)){
+//                    System.out.println("Does not have <var>");
+//                    break;
+//                }
+//                if (input.charAt(i) == ')' && input.charAt(i + 1) == '{') {
+//                    System.out.println(var + "\nreached");
+//                    pointer = i + 2; // brings to pointer after {
+//                    break;
+//                }
+//            }
+//
+//            //case: checker
+//            for (int i = pointer; i < input.length(); i++) {
+//
+//                if (input.charAt(i) == 'c' && input.charAt(i + 1) == 'a' && input.charAt(i + 2) == 's'
+//                        && input.charAt(i + 3) == 'e') {
+//                    System.out.println("goods sa case");
+//                    i += 3;
+//                    pointer = i;
+//                    break;
+//                }
+//            }
+//
+//            for (int i = pointer; i < input.length(); i++) {
+//                if ((input.charAt(i) >= 'a' && input.charAt(i) <= 'z') || (input.charAt(i) >= 0 && input.charAt(i) <= 9) && (input.charAt(i+1) == ':')) {
+//                    System.out.println("goods sa case number or char at colon");
+//                    break;
+//                }
+//            }
+//
+//            /*
+//             * int mark = 8;
+//             * String temp = "temp";
+//             * for (int i = mark; temp != ""; i++) {
+//             * if (i == input.length()) {
+//             * System.out.println("This is not a valid loop declaration");
+//             * System.exit(0);
+//             * }
+//             * temp = "" + input.charAt(i);
+//             * if (temp.equals(")")) {
+//             * mark = i;
+//             * break;
+//             * }
+//             * }
+//             *
+//             * mark++;
+//             * temp = "temp";
+//             * for (int i = mark; temp != ""; i++) {
+//             * if (i == input.length()) {
+//             * System.out.println("This is not a valid loop declaration");
+//             * System.exit(0);
+//             * }
+//             * }
+//             */
+//        } else {
+//            System.out.println("This is not a valid switch declaration");
+//        }
     }
 }
