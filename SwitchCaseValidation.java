21408f8a33391bbae4a43dde19785c01dddffe60
Shaun-PJuan <shaunkristoffer.padrejuan.cics@ust.edu.ph>
2023-10-16 21:04:38 +0800

    Added the case and break checker.

--- SwitchCaseValidation.java
+++ SwitchCaseValidation.java
@@ -1,99 +1,158 @@
 import java.util.Scanner;
-
+import java.util.regex.Pattern;
 public class SwitchCaseValidation {
-//    Method to check for switch reserved word. Returns true if there is an instance of the word
+    //    Method to check for switch reserved word. Returns true if there is an instance of the word
 //    Return false if no instance is found
-    public static boolean switchChecker(String input){
+    public static boolean switchChecker(String input) {
         String switchFinder = "";
-        for (int i = 0; i<input.length(); i++){
-            if (switchFinder.equals("switch")){
+        for (int i = 0; i < input.length(); i++) {
+            if (switchFinder.equals("switch")) {
                 System.out.println("Switch keyword found!");
                 return true;
-            }
-            else
+            } else
                 switchFinder = switchFinder + input.charAt(i);
         }
         System.out.println("Switch keyword not found!");
         return false;
     }
-//    Method to check the variable inside the switch()
+
+    //    Method to check the variable inside the switch()
 //    Note: switchChecker must be true to test this next
-    public static boolean switchVariableChecker(String input){
-//        Finds the first instance of open parenthesis
+    public static boolean switchVariableChecker(String input) {
         String switchVariableFinder = "";
         int firstParenthesis = 0;
-        for (int i = 0; i < input.length(); i++){
-            if (input.charAt(i) == '('){
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
                 firstParenthesis = i + 1;
                 break;
             }
-            if (input.charAt(i) == ')'){
+            if (input.charAt(i) == ')') {
                 System.out.println("No opening parenthesis found for <var>!");
-                return false; // no opening parenthesis found before closing
+                return false;
             }
         }
 //        Loop that starts from the index of the first parenthesis after the switch keyword
-        for (int i = firstParenthesis; i < input.length();i++) {
-            if (input.charAt(i) == '{'){
+        for (int i = firstParenthesis; i < input.length(); i++) {
+            if (input.charAt(i) == '{') {
                 switchVariableFinder = switchVariableFinder + input.charAt(i);
                 break;
-            }
-            else
+            } else
                 switchVariableFinder = switchVariableFinder + input.charAt(i);
         }
-        System.out.println(switchVariableFinder);
 
 //        if the <var> is just whitespace or is null
         if (switchVariableFinder.charAt(0) == ')') {
             System.out.println("No arguments found for switch!");
-            return false; //no variable found in argument
+            return false;
         }
 
-//      if the <var> has illegal characters inside parenthesis
 //      TODO: add more illegal characters, functions inside parenthesis?
-        if (!switchVariableFinder.isBlank()){
+        if (!switchVariableFinder.isBlank()) {
             int closingParenthesisCounter = 0;
-            for (int i = 0; i < switchVariableFinder.length()-1; i++) { // to not include the starting curly brace
+            for (int i = 0; i < switchVariableFinder.length() - 1; i++) { // to not include the starting curly brace
                 if (switchVariableFinder.charAt(i) == '{' || switchVariableFinder.charAt(i) == '}'
                         || switchVariableFinder.charAt(i) == '(') {
                     System.out.println("Illegal characters for <var>!");
-                    return false; // illegal character found
+                    return false;
                 }
-                if (switchVariableFinder.charAt(i) == ')'){
+                if (switchVariableFinder.charAt(i) == ')') {
                     closingParenthesisCounter++;
                 }
             }
-            if (closingParenthesisCounter > 1){
+            if (closingParenthesisCounter > 1) {
                 System.out.println("Too many closing parenthesis!");
-                return false; // valid variable argument
+                return false;
             }
             System.out.println("Valid <var> argument!");
-            return true; // valid variable argument
+            return true;
         }
         System.out.println("No closing parenthesis found for <var>!");
         return false; //default return statement
     }
 
     //TODO: implement these methods
-    public static boolean curlyBraceChecker (String input){
+//    Method to check for correct case syntax and break statements
+    public static boolean caseAndBreakChecker(String input) {
+        String findCase = "case";
+        int numberOfCases = (int) Pattern.compile(findCase).matcher(input).results().count();
+        int caseStart = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '{') {
+                caseStart = i + 1;
+                break;
+            }
+        }
+        int caseOccurence = input.indexOf("case", caseStart);
+        int breakOccurence = input.indexOf("break;", caseOccurence);
+        for (int i = 1; i < numberOfCases + 1; i++){
+            for (int y = caseOccurence; y < input.length(); y++){
+                if (input.charAt(y) == ':'){
+                    if (caseOccurence > breakOccurence){
+                        System.out.println("WARNING: Case " + i + " has no break statement");
+                        caseOccurence = input.indexOf("case", y);
+                    }
+                    else if (caseOccurence < breakOccurence){
+                        caseOccurence = input.indexOf("case", y);
+                        int breakDoubleCheck = input.indexOf("break;", caseOccurence);
+                        if (breakDoubleCheck == breakOccurence){
+                            System.out.println("WARNING: Case " + i + " has no break statement");
+                        }
+                        else{
+                            System.out.println("Case " + i + " found with break statement!");
+                            breakOccurence = input.indexOf("break;", caseOccurence);
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean defaultChecker(String input) {
         return false;
     }
-    public static boolean caseAndBreakChecker (String input){
+
+    public static boolean endingCurlyBraceChecker(String input) {
         return false;
     }
-    public static boolean defaultChecker (String input){
-        return false;
-    }
-    public static boolean endingCurlyBraceChecker (String input){
-        return false;
-    }
+
     public static void main(String[] args) {
-        String input = "switch(test){case 1: System.out.println(\"1\"); \n break;\n}";
-        System.out.println("Input: " + input);
-        if (switchChecker(input) && switchVariableChecker(input))
-            System.out.println("The input is a valid switch statement");
-        else
+        String input = "switch(test){\ncase 'a': System.out.println(\"1\"); \n break;\n" +
+                "case 'b': System.out.println(\"2\"); \n break;\n" +
+                "case 'c': System.out.println(\"3\"); \n break;\n" +
+                "case 'd': System.out.println(\"4\"); \n break;\n}";
+//        System.out.println("Input: " + input);
+//        if (switchChecker(input))
+//            System.out.println("The input has the switch keyword");
+//        else {
+//            System.out.println("The input is an invalid switch statement");
+//            System.exit(0);
+//        }
+//        if (switchVariableChecker(input))
+//            System.out.println("The input has a valid <var> argument");
+//        else {
+//            System.out.println("The input is an invalid switch statement");
+//            System.exit(0);
+//        }
+
+        if (caseAndBreakChecker(input))
+            System.out.println("The input has a valid cases syntax");
+        else {
             System.out.println("The input is an invalid switch statement");
+            System.exit(0);
+        }
+//        if (defaultChecker(input))
+//            System.out.println("The input has a default code block");
+//
+//        if (endingCurlyBraceChecker(input))
+//            System.out.println("The input has an ending curly brace.");
+//        else {
+//            System.out.println("The input is an invalid switch statement");
+//            System.exit(0);
+//        }
+
+
 //        String forCheck = "" + input.charAt(0) + input.charAt(1) + input.charAt(2) +
 //                input.charAt(3) + input.charAt(4) + input.charAt(5) + input.charAt(6);
 //
@@ -198,3 +257,4 @@ public class SwitchCaseValidation {
 //        }
     }
 }
+
